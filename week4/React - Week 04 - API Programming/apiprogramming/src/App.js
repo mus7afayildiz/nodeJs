a26dade4f3e80aed17e49b534590b048aa9e1965
mus7afayildiz <mus7afayildiz@yandex.com>
2022-03-13 02:06:15 +0100

    logincomponent is edited

--- week4/React - Week 04 - API Programming/apiprogramming/src/App.js
+++ week4/React - Week 04 - API Programming/apiprogramming/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  
+  const data = async () => {
+    try{
+    const alinanApi = await fetch("https://api.chucknorris.io/jokes/random");
+    const yazilacakapi = await alinanApi.json();
+    console.log(yazilacakapi) 
+   
+    }catch (error){
+    console.log("hata")
+  } 
+  }
+  data()
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
